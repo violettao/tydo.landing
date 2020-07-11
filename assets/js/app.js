62f73031b0dc5dd88aa0e11c755d2ed9581e05b0
Violetta <oliinyk.violetta@gmail.com>
2020-07-11 09:49:43 +0300

    added tabs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,24 @@
+//tabs block 03
+
+$(document).ready(function() {
+    $("#content div").hide(); // Скрываем содержание
+    $("#tabs button:first").attr("id","current"); // Активируем первую закладку
+    $("#content div:first").fadeIn(); // Выводим содержание
+
+    $('#tabs button').click(function(e) {
+        e.preventDefault();
+        $("#content div").hide(); //Скрыть все сожержание
+        $("#tabs button").attr("id",""); //Сброс ID
+        $(this).parent().attr("id","current"); // Активируем закладку
+        $('#' + $(this).attr('title')).fadeIn(); // Выводим содержание текущей закладки
+    });
+});
+
+
+
+
+//styled card block 05
+
 jQuery(function ($) {
     $('.card-05').masonry({
         itemSelector: '.card-item',
